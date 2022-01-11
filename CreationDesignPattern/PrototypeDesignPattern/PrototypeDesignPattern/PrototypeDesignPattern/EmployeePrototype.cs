8cfa8e2439770fb9dd7de49f7f32c5499d8babc3
Antonius Atef Ghaly <antonius.a.ghaly@gmail.com>
2022-01-11 17:01:34 +0200

    prototype p1

---
+++ CreationDesignPattern/PrototypeDesignPattern/PrototypeDesignPattern/PrototypeDesignPattern/EmployeePrototype.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeDesignPattern
+{
+    public abstract class EmployeePrototype
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }  
+        public Address address { get; set; }
+        public abstract EmployeePrototype ShalowCopy();
+        public abstract EmployeePrototype DeepCopy();
+        public override string ToString()
+        {
+            return   
+$@"Id={Id}
+Name : {Name}  
+Address : {address.City},{address.street},{address.building}";
+        }
+    }
+}
---
+++ CreationDesignPattern/PrototypeDesignPattern/PrototypeDesignPattern/PrototypeDesignPattern/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PrototypeDesignPattern // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region shallow copy
+            EmployeePrototype tempEmployee1 = new TempEmployee();
+            tempEmployee1.Name = "antonius atef";
+            tempEmployee1.Id = 1;
+            tempEmployee1.address = new Address() { City="cairo",street="zahraa",building="1"};
+
+            //create copy of emp1
+            EmployeePrototype tempEmployee2 = tempEmployee1.ShalowCopy();
+           
+            Console.WriteLine("-----------original employee 1  -----------------");
+            Console.WriteLine(tempEmployee1.ToString());
+            Console.WriteLine("----------- employee 2  -------------------------");
+            Console.WriteLine(tempEmployee2.ToString());
+
+            tempEmployee2.Name = "emp2";
+            tempEmployee2.Id = 2;
+            tempEmployee2.address.City = "city2";
+            tempEmployee2.address.street = "street 2";
+            tempEmployee2.address.building = "building 2";
+
+            Console.WriteLine("-----------original employee 1 after change 2  -----------------");
+            Console.WriteLine(tempEmployee1.ToString());
+            Console.WriteLine("----------- employee 2  after change   -------------------------");
+            Console.WriteLine(tempEmployee2.ToString());
+            #endregion
+
+            #region deep copy
+
+            #endregion
+        }
+    }
+}

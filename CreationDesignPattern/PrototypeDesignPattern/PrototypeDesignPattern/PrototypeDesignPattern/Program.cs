1d2bb2bcecbda54e3b4cb4c92f2f7e11c3511dd0
Antonius Atef Ghaly <antonius.a.ghaly@gmail.com>
2022-01-12 15:24:55 +0200

    prototype pattern p2

--- CreationDesignPattern/PrototypeDesignPattern/PrototypeDesignPattern/PrototypeDesignPattern/Program.cs
+++ CreationDesignPattern/PrototypeDesignPattern/PrototypeDesignPattern/PrototypeDesignPattern/Program.cs
@@ -31,9 +31,30 @@ namespace PrototypeDesignPattern // Note: actual namespace depends on the projec
             Console.WriteLine("----------- employee 2  after change   -------------------------");
             Console.WriteLine(tempEmployee2.ToString());
             #endregion
-
             #region deep copy
+            EmployeePrototype tempEmployee3 = new TempEmployee();
+            tempEmployee1.Name = "antonius atef";
+            tempEmployee1.Id = 1;
+            tempEmployee1.address = new Address() { City = "cairo", street = "zahraa", building = "1" };
 
+            //create copy of emp1
+            EmployeePrototype tempEmployee4 = tempEmployee1.DeepCopy();
+
+            Console.WriteLine("-----------original employee 1  -----------------");
+            Console.WriteLine(tempEmployee3.ToString());
+            Console.WriteLine("----------- employee 2  -------------------------");
+            Console.WriteLine(tempEmployee4.ToString());
+
+            tempEmployee4.Name = "emp2";
+            tempEmployee4.Id = 2;
+            tempEmployee4.address.City = "city2";
+            tempEmployee4.address.street = "street 2";
+            tempEmployee4.address.building = "building 2";
+
+            Console.WriteLine("-----------original employee 3 after change 2  -----------------");
+            Console.WriteLine(tempEmployee3.ToString());
+            Console.WriteLine("----------- employee 4  after change   -------------------------");
+            Console.WriteLine(tempEmployee4.ToString());
             #endregion
         }
     }

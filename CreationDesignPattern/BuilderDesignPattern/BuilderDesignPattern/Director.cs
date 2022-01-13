d7c0ae98f0311961f580b5a960bc264d1941fc70
Antonius Atef Ghaly <antonius.a.ghaly@gmail.com>
2022-01-13 16:11:18 +0200

    builder design pattern

---
+++ CreationDesignPattern/BuilderDesignPattern/BuilderDesignPattern/Director.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderDesignPattern
+{
+    internal class Director
+    {
+        IBuilder builder;
+        public void Construct(IBuilder builder)
+        {
+            this.builder = builder;
+            builder.StartOperation();
+            builder.BuildBody();
+            builder.InsertWheels();
+            builder.AddHeadLights();
+            builder.EndOperation();
+        }
+    }
+}

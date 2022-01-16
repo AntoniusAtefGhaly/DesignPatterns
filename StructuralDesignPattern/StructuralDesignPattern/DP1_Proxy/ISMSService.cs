beb472e01a9c4e9b97a5c097182a04d25775c78f
Antonius Atef Ghaly <antonius.a.ghaly@gmail.com>
2022-01-16 16:54:28 +0200

    proxy design pattern

---
+++ StructuralDesignPattern/StructuralDesignPattern/DP1_Proxy/ISMSService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP1_Proxy
+{
+    internal interface ISMSService
+    {
+        void SendSMS(string Sender,string reciever,string SMS);
+    }
+}
---
+++ StructuralDesignPattern/StructuralDesignPattern/DP1_Proxy/SMSService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP1_Proxy
+{
+    internal class SMSService : ISMSService
+    {
+        public void SendSMS(string Sender, string reciever, string SMS)
+        {
+            Console.WriteLine("*********************sms***********************");
+            Console.WriteLine($"{SMS}\nsender: {Sender} TO {reciever}\nSUCCUFULLY SENT");
+            Console.WriteLine("************************************************");
+        }
+    }
+}
